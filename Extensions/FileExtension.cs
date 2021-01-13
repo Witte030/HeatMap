9e064f84ce6b15c8884a31465faf0b17fb693b76
Martijn Heida <mcheida7@hotmail.com>
2021-01-13 21:10:38 +0100

    Initial commit

---
+++ Extensions/FileExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace HeatMap.Extensions
+{
+    public class FileExtension
+    {
+        /// <summary>
+        /// Get file path with extension
+        /// </summary>
+        /// <param name="fileType">File type</param>
+        /// <param name="fileExtension">File extension</param>
+        /// <returns>Complete file path with name and extension</returns>
+        public static string GetFilePathWithExtension(string fileType, string fileExtension)
+        {
+            string output = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog
+                { Filter = $"{fileType}(*.{fileExtension}) | *.{fileExtension}" };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                output = openFileDialog.FileName;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Get folder path
+        /// </summary>
+        /// <returns>Full folder path</returns>
+        public static string GetFolderPath()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            return dialog.SelectedPath;
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,84 @@
+﻿using HeatMap.Extensions;
+using Prism.Commands;
+using Prism.Mvvm;
+using Squirrel;
+
+namespace HeatMap.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Heat Map";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+           CheckForUpdate();
+        }
+
+        private string _csvPath;
+
+        public string CsvPath
+        {
+            get => _csvPath;
+            set
+            {
+                SetProperty(ref _csvPath, value);
+                ConvertCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private DelegateCommand _getCsvPathCommand;
+
+        public DelegateCommand GetCsvPathCommand =>
+            _getCsvPathCommand ?? (_getCsvPathCommand = new DelegateCommand(ExecuteGetCsvPathCommand));
+
+        void ExecuteGetCsvPathCommand()
+        {
+            CsvPath = FileExtension.GetFilePathWithExtension("csv", "csv");
+        }
+
+        private DelegateCommand _convertCommand;
+
+        public DelegateCommand ConvertCommand =>
+            _convertCommand ?? (_convertCommand = new DelegateCommand(ExecuteConvertCommand, CanExecuteConvertCommand));
+
+        void ExecuteConvertCommand()
+        {
+
+        }
+
+        bool CanExecuteConvertCommand()
+        {
+
+            return !string.IsNullOrWhiteSpace(CsvPath);
+        }
+
+        private async void CheckForUpdate()
+        {
+            using (var mgr = new UpdateManager("D:\\HeatMap"))
+            {
+                await mgr.UpdateApp();
+            }
+            //try
+            //{
+            //    using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/Witte030/HeatMapTest"))
+            //    {
+            //        await mgr.Result.UpdateApp();
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    string message = ex.Message + System.Environment.NewLine;
+            //    if (ex.InnerException != null)
+            //        message += ex.InnerException.Message;
+            //    MessageBox.Show(message);
+            //}
+        }
+
+    }
+}
+
